9a32629f35b0419665bd2d8de4ce50296c46f47b
Patrice Chalin <pchalin@gmail.com>
2018-01-24 19:11:44 -0500

    Sync with f5727ce
    Synced with dart-lang/site-webdev 5-dev branch, commit f5727ce:
    https://github.com/dart-lang/site-webdev/tree/f5727cee4580ff51dc3ba90d176608daa8c6adca/examples/ng/doc/server-communication

--- web/main.dart
+++ web/main.dart
@@ -1,19 +1,28 @@
 import 'package:angular/angular.dart';
-import 'package:server_communication/app_component.dart';
-import 'package:server_communication/in_memory_data_service.dart';
+/*
+import 'package:http/browser_client.dart';
+*/
 import 'package:http/http.dart';
+import 'package:server_communication/app_component.dart';
+
+import 'main.template.dart' as ng;
+import 'package:server_communication/in_memory_data_service.dart';
 
 void main() {
-  bootstrap(AppComponent, [
-    // in-memory web api provider
-    provide(Client, useClass: InMemoryDataService)
-  ]);
+  bootstrapStatic(
+      AppComponent,
+      [
+        // in-memory web api provider
+        provide(Client, useClass: InMemoryDataService)
+      ],
+      ng.initReflector);
 }
 /*
 
 void main() {
-  bootstrap(AppComponent, [
-    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
-  ]);
+  bootstrapStatic(AppComponent, [
+    provide(Client, useFactory: () => new BrowserClient(), deps: [])
+  ],
+  ng.initReflector);
 }
 */
